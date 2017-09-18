1e1df2d179a356a91de39735202dfa641b9dcaf7
mrchebik <mrchebik@yandex.ru>
2017-09-18 03:50:07 +0300

    added exitOnSec

--- src/main/java/ru/mrchebik/Main.java
+++ src/main/java/ru/mrchebik/Main.java
@@ -3,11 +3,26 @@ package ru.mrchebik;
 import javafx.application.Application;
 import ru.mrchebik.gui.places.start.StartPlace;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by mrchebik on 07.05.16.
  */
 public class Main {
     public static void main(String[] args) {
+        if ("-exitOnSec".equals(args[0]))
+            if (args[1] != null) {
+                Timer timer = new Timer();
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        System.exit(0);
+                    }
+                };
+                timer.schedule(timerTask, Integer.parseInt(args[1]) * 1000);
+            }
+
         Application.launch(StartPlace.class);
     }
 }
