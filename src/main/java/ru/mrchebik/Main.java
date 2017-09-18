8d4053c02beb6d72ca14e04a93f7acfa64f0dfe2
mrchebik <mrchebik@yandex.ru>
2017-09-18 09:23:06 +0300

    improve args

--- src/main/java/ru/mrchebik/Main.java
+++ src/main/java/ru/mrchebik/Main.java
@@ -11,17 +11,16 @@ import java.util.TimerTask;
  */
 public class Main {
     public static void main(String[] args) {
-        if ("-exitOnSec".equals(args[0]))
-            if (args[1] != null) {
-                Timer timer = new Timer();
-                TimerTask timerTask = new TimerTask() {
-                    @Override
-                    public void run() {
-                        System.exit(0);
-                    }
-                };
-                timer.schedule(timerTask, Integer.parseInt(args[1]) * 1000);
-            }
+        if (args.length > 1 && "-exitOnSec".equals(args[0])) {
+            Timer timer = new Timer();
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    System.exit(0);
+                }
+            };
+            timer.schedule(timerTask, Integer.parseInt(args[1]) * 1000);
+        }
 
         Application.launch(StartPlace.class);
     }
